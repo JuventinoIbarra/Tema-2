b834d227245e379d6a3f2976dff3ea4dca5b5c77
Juventino Ibarra <al07133783@tecmilenio.mx>
2026-03-18 14:54:37 -0600

    Funcionalidad del botón

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-action").addEventListener("click", function()
+{
+    const mensaje = document.getElementById('mensaje');
+
+    mensaje.innerHTML = "¡Código esta funcionando!";
+    mensaje.style.color = "green";
+
+    console.log("Evento de clik al botón");
+});
